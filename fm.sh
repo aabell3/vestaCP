4c379febaa9cb85862de6551a68a7bdaef608b79
Barock <aabell3@wgu.edu>
2017-09-08 09:43:01 +0700

    Create fm.sh

---
+++ fm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# go to root
+cd
+wget -O /usr/local/vesta/conf/vesta.conf "https://raw.githubusercontent.com/aabell3/vestaCP/master/vesta.conf"
+cd
+echo "VestaCP File Manager Installed By Tukiyem All Aluwallah"
